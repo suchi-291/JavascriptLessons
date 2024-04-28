bbd977ca4f67676469b6e6583d74e1619c243c18
Suchitha Ramachandrapurapu <66826980+suchi-291@users.noreply.github.com>
2024-04-28 20:14:34 +0530

    Add files via upload

---
+++ day2/script.js
@@ -0,0 +1,58 @@
+var my_name = "Suchitha";
+console.log(my_name);
+
+var my_name = "Ravali";
+console.log(my_name);
+
+//______________________________________________
+
+let her_name = "chitra";
+console.log(her_name);
+
+//let her_name = "chaitra";
+//console.log(her_name);
+her_name = "chaitra";
+console.log(her_name);
+
+// __________________________________________________
+
+const name = "suriya";
+
+console.log(name);
+
+//name = "aravind";
+// console.log(name);
+
+
+//prompt("What is your name?");
+
+//var first = prompt("Enter first number");
+//var second = prompt("Enter Second number");
+
+//var sum = Number(first) + Number(second);
+//console.log(sum);
+//alert(sum);
+
+document.write("This is javasript text message <br>");
+
+document.write(12345678);
+
+document.write("<h1> This is my heading text </h1>");
+
+
+
+
+
+
+
+//____________________________________________________________
+
+
+var firstNumber = prompt("Please enter the first number: ");
+var secondNumber = prompt("Please enter the second number: ");
+var sum = Number(firstNumber) + Number(secondNumber);
+var subtract =  Number(firstNumber) - Number(secondNumber);
+var product =  Number(firstNumber) * Number(secondNumber);
+var divide =  Number(firstNumber) / Number(secondNumber);
+alert(" The sum of the given two numbers is: " + sum + "\n" + "The difference of the given two numbers is: " + subtract + "\n" + "The product of the given two numbers is: " + product + "\n" + "The quotient of the two numbers is: " + divide);
+
